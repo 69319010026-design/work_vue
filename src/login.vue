658c85bf502bf538c3b53451fa22c9637e88ebfc
Rakachai <69319010043@udontech.ac.th>
2026-07-13 16:01:21 -0700

    Inital commit

--- src/login.vue
+++ src/login.vue
@@ -2,14 +2,42 @@
     <div class="main-wrapper">
         <h1>สมัครสมาชิก</h1>
         <form @submit="alert_data">
-          <div class="form-group">
-            <label>ชื่อ:</label>
-            <input type="text" v-model="name_box" placeholder="กรอกชื่อ"/>
-          </div>
-          <div class="form-group">
-            <label>นามสกุล:</label>
-            <input type="text" v-model="surname_box" placeholder="กรอกนามสกุล"/>
-          </div>
+            <div class="form-group">
+                <label>ชื่อ:</label>
+                <input type="text" v-model="name_box" placeholder="กรอกชื่อ" />
+            </div>
+            <div class="form-group">
+                <label>นามสกุล:</label>
+                <input type="text" v-model="surname_box" placeholder="กรอกนามสกุล" />
+            </div>
+            <div class="form-group">
+                <label>เพศ:</label>
+                <label>
+                    <input type="radio" name="sex" v-model="sex" value="male"> ชาย
+                </label>
+
+                <label>
+                    <input type="radio" name="sex" v-model="sex" value="female"> หญิง
+                </label>
+            </div>
+            <div class="form-group">
+                <label>งานอดิเรก</label>
+                <label>
+                    <input type="checkbox" name="favorite" v-model="favorite" value="เลี้ยงสัตว์"> เลี้ยงสัตว์
+                </label>
+
+                <label>
+                    <input type="checkbox" name="favorite" v-model="favorite" value="วาดรูป"> วาดรูป
+                </label>
+
+                <label>
+                    <input type="checkbox" name="favorite" v-model="favorite" value="เล่นดนตรี"> เล่นดนตรี
+                </label>
+
+                <label>
+                    <input type="checkbox" name="favorite" v-model="favorite" value="ต่อยมวย"> ต่อยมวย
+                </label>
+            </div>
             <div class="form-group">
                 <button type="submit">Click</button>
 
@@ -27,9 +55,11 @@
 import { ref } from 'vue';
 const name_box = ref('');
 const surname_box = ref('');
+const sex = ref('');
+const favorite = ref([])
 const alert_data = () => {
-alert('ยินดีต้อนรับ คุณ' + name_box.value + ' '+surname_box.value);
-    
+    alert('ยินดีต้อนรับ คุณ' + name_box.value + ' ' + surname_box.value + ' ' + 'เพศ' + sex.value + ' ' + 'งานอดิเรก' + favorite.value);
+
 }
 </script>
 <style scoped>
@@ -49,7 +79,8 @@ alert('ยินดีต้อนรับ คุณ' + name_box.value + ' '+su
     font-size: larger;
     color: brown;
 }
-.form-group{
+
+.form-group {
     margin-bottom: 15px;
 }
 </style>
